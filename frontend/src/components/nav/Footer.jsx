4112b8649c8e4deb169d6a342857fce9cb0d5f0d
Juan21Herrera <juanherrera0240@gmail.com>
2025-06-04 15:51:06 -0500

    change footer color

--- frontend/src/components/nav/Footer.jsx
+++ frontend/src/components/nav/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer class="bg-[#fbabfc] text-white p-4 text-center ">
+    <footer class="bg-blue-600 text-white p-4 text-center ">
       <p>&copy; 2025 Mi Sitio. Todos los derechos reservados.</p>
       <div class="mt-2 space-x-4">
         <a href="#" class="hover:underline">
